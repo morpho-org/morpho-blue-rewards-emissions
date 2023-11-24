62b7fcdcd7f0103fa3fbb9305124a2ff7d635f56
julien <julien@morpho.xyz>
2023-11-24 15:39:13 +0100

    feat: change way we are designing data provision

--- README.md
+++ README.md
@@ -3,21 +3,14 @@
 This contract aims to give a simple way to track emissions of a given ERC20 token over a rewards program on top of Morpho Blue.
 
 Rewards emissions are organized under the following structure:
-- Rewards Program ID: this is an unique identifier for a given rewards program managed by a DAO for example. 
-- Token: The token address that is being rewarded in the program. You can have multiple tokens per program.
-- market: The market id that is being rewarded in the program. You can have multiple markets per program.
-- RewardsEmission: The rewards struct containing the emission rate of rewards, the start date and the end date. 
+- `Token`: The token address that is being rewarded in the program. You can have multiple tokens per program.
+- `Market`: The market id that is being rewarded in the program. You can have multiple markets per program.
+- `RewardsEmission`: The rewards struct containing the emission rate of rewards for each side of a Morpho market (supply, borrow, collateral). 
 
 
 ## Specs
 
-- The owner should be able to update the rewards emission rate for a given program, token and market.
-- The start date can be only in the future. 
-- The end date is optional and can be updated later.
-
-
-Problem: how to handle the case where the owner wants to update the start date of a rewards emission that has already started?
-Problem: how to handle the case where we want to push the next rate in the future, without overriding the current one?
-
-Solution: use a round based system. Each time the owner updates the rewards emission, a new round is created.
-The owner can update the start date of future round, the end date of the current one, and push new rounds in the future.
+- The owner should be able to update the rewards emission rate for a given token and market.
+- The rate is valid at the block timestamp of the rate update. 
+- To stop a rewards distribution, the owner should set the rate to 0.
+- we are not checking if the market exists on blue. It is up to the owner to well configure the distributions.
--- src/EmissionDataProvider.sol
+++ src/EmissionDataProvider.sol
@@ -1,50 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.21;
 
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {Multicall} from "openzeppelin/utils/Multicall.sol";
 
 struct RewardsEmission {
-
-    /// @notice the date at which the emission starts
-    uint256 startAt;
-
-    /// @notice the date at which the emission ends
-    /// @dev if 0, the emission is considered to not have end date for now.
-    uint256 endAt;
-
-    /// @notice the amount of tokens to be emitted per year (365 days)
-    uint256 ratePerYear;
+    uint256 supplyRatePerYear;
+    uint256 borrowRatePerYear;
+    uint256 collateralRatePerYear;
 }
 
-contract EmissionDataProvider is Ownable {
+contract EmissionDataProvider is Ownable, Multicall {
+    /// @notice reward token -> market -> RewardsEmission mapping
+    mapping(address => mapping(bytes32 => RewardsEmission)) public rewardsEmissions;
 
+    address public immutable REWARDS_DISTRIBUTOR;
 
-    /// @notice rewards program id -> token -> market -> RewardsEmission mapping
-    mapping(uint256 => mapping(address => mapping(bytes32 => RewardsEmission))) public rewardsEmissions;
+    event RewardsEmissionSet(address indexed token, bytes32 indexed market, RewardsEmission rewardsEmission);
 
-    event RewardsEmissionSet(uint256 indexed id, address indexed token, bytes32 indexed market, RewardsEmission rewardsEmission);
-
-    constructor() Ownable() {}
-
-    function setRewardsEmission(uint256 id, address token, bytes32[] memory markets, RewardsEmission[] memory emissions) public onlyOwner {
-        require(markets.length == emissions.length, "EmissionDataProvider: markets and emissions length mismatch");
-
-        for (uint256 i = 0; i < markets.length; i++) {
-            require(emissions[i].startAt >= block.timestamp, "EmissionDataProvider: emission startAt must be greater than current timestamp");
-            require(emissions[i].endAt == 0 || emissions[i].endAt > emissions[i].startAt, "EmissionDataProvider: emission endAt must be greater than startAt");
-
-            rewardsEmissions[id][token][markets[i]] = emissions[i];
-
-            emit RewardsEmissionSet(id, token, markets[i], emissions[i]);
-        }
+    constructor(address rewardsDistributor, address initialOwner) Ownable(initialOwner) Multicall() {
+        REWARDS_DISTRIBUTOR = rewardsDistributor;
     }
 
-    function addStopDate(uint256 id, address token, bytes32 market, uint256 stopDate) public onlyOwner {
-        require(stopDate > block.timestamp, "EmissionDataProvider: stopDate must be greater than current timestamp");
-        require(rewardsEmissions[id][token][market].startAt > 0, "EmissionDataProvider: emission not found");
+    function setRewardsEmission(address token, bytes32 market, RewardsEmission calldata rewardsEmission)
+        public
+        onlyOwner
+    {
+        rewardsEmissions[token][market] = rewardsEmission;
 
-        rewardsEmissions[id][token][market].endAt = stopDate;
+        emit RewardsEmissionSet(token, market, rewardsEmission);
     }
-
-
 }
