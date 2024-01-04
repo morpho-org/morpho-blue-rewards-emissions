b47735edc40dd2ddc611d921e2b71ef89c1288f7
MerlinEgalite <egalite.merlin@gmail.com>
2024-01-04 18:58:18 +0100

    refactor: update docs + naming

--- src/EmissionDataProvider.sol
+++ src/EmissionDataProvider.sol
@@ -1,22 +1,27 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {Multicall} from "openzeppelin/utils/Multicall.sol";
 import {Id} from "morpho-blue/interfaces/IMorpho.sol";
 
+import {Multicall} from "openzeppelin/utils/Multicall.sol";
+
 struct RewardsEmission {
+    /// @notice The rewards rate per year for the supply side.
     uint256 supplyRatePerYear;
+    /// @notice The rewards rate per year for the borrow side.
     uint256 borrowRatePerYear;
+    /// @notice The rewards rate per year for the collateral side.
     uint256 collateralRatePerYear;
 }
 
 contract EmissionDataProvider is Multicall {
-    /// @notice reward token -> market -> RewardsEmission mapping
+    /// @notice Returns the rewards emission. sender -> urd -> token -> market -> rewardsEmission.
     mapping(
         address sender
             => mapping(address urd => mapping(address rewardToken => mapping(Id marketId => RewardsEmission)))
     ) public rewardsEmissions;
 
+    /// @notice Emitted when the rewards emission is set.
     event RewardsEmissionSet(
         address indexed rewardToken,
         Id indexed market,
@@ -25,11 +30,16 @@ contract EmissionDataProvider is Multicall {
         RewardsEmission rewardsEmission
     );
 
-    function setRewardsEmission(address token, address urd, Id market, RewardsEmission calldata rewardsEmission)
+    /// @notice Sets the rewards emission.
+    /// @param rewardToken The reward token of the emission.
+    /// @param urd The URD distributing the rewards.
+    /// @param market The id of market on which rewards are distributed.
+    /// @param rewardsEmission The rewards emission to set.
+    function setRewardsEmission(address rewardToken, address urd, Id market, RewardsEmission calldata rewardsEmission)
         public
     {
-        rewardsEmissions[msg.sender][urd][token][market] = rewardsEmission;
+        rewardsEmissions[msg.sender][urd][rewardToken][market] = rewardsEmission;
 
-        emit RewardsEmissionSet(token, market, msg.sender, urd, rewardsEmission);
+        emit RewardsEmissionSet(rewardToken, market, msg.sender, urd, rewardsEmission);
     }
 }
