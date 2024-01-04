df75ac6f8e129f8f91726e8a067812165f7bcc6c
MerlinEgalite <egalite.merlin@gmail.com>
2024-01-04 18:57:59 +0100

    script: very simple deployment script that works anyway

---
+++ script/Deploy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import "forge-std/Script.sol";
+import "src/EmissionDataProvider.sol";
+
+contract Deploy is Script {
+	bytes32 internal constant SALT = bytes32(0);
+
+	function run() public {
+		vm.broadcast();
+		EmissionDataProvider dataProvider = new EmissionDataProvider{salt: SALT}();
+		console2.log("EmissionDataProvider deployed at address: ", address(dataProvider));
+	}
+}
